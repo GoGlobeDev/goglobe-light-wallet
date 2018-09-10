abe8bb2e382bb2bb0d0c939857b129f46fa2a460
Leexuetao <15102206169@163.com>
2018-09-10 11:40:48 +0800

    测试地址

--- app/utils/config.js
+++ app/utils/config.js
@@ -1,7 +1,7 @@
-// const serverUrl = 'http://45.40.243.125:7001/'; // 测试地址
-// const serverUrl = 'http://39.105.125.189:7001/'; // 正式地址
 
-const serverUrl = 'http://54.250.199.29:8088/'; //合约地址
+
+// const serverUrl = 'http://54.250.199.29:8088/'; //合约地址 正式地址
+const serverUrl = 'http://13.112.45.51:8088/'; //合约地址 测试地址
 
 // const hostMode = 'ropsten';
 const hostMode = 'mainnet';
